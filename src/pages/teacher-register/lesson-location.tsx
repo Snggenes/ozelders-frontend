72394bac57cca8b30fdf1df28974e46e25163c82
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2024-09-05 19:54:00 +0200

    chars

--- src/pages/teacher-register/lesson-location.tsx
+++ src/pages/teacher-register/lesson-location.tsx
@@ -9,6 +9,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Checkbox } from "@/components/ui/checkbox";
+import LeftSide from "./left-side";
 
 export default function LessonLocation({ form }: { form: any }) {
   const city = form.watch("city");
@@ -32,6 +33,29 @@ export default function LessonLocation({ form }: { form: any }) {
 
     fetchDistricts();
   }, [city]);
+
+
+  const childNode =      <div className=" md:mr-16 md:w-full bg-rose-100 rounded-3xl flex flex-col md:order-1 order-2 p-6  ">
+  <Heading title="Bilgi" classNameTitle=" text-5xl mb-6"></Heading>
+  <div>
+    <br />
+    <p>
+      Lütfen ders verebileceginiz ilçeleri seçin. İstediğiniz kadar seçim
+      yapabilirsiniz.
+    </p>
+    <br />
+    <p>
+      Bu bilgiyle sizin ders verebileceğiniz ilçelerdeki öğrencilerle
+      iletişim kurabileceksiniz.
+    </p>
+    <br />
+    <p>
+      Hem siz seçtiğiniz ilçelerdeki öğrencilere gösterileceksiniz, hem de
+      ders taleplerini görürken bu ilçelerdeki kişilere ulaşmış
+      olacaksınız.
+    </p>
+  </div>
+</div>
 
   return (
     <div className="flex flex-col md:px-32  md:py-16 md:flex-row gap-4 md:m-6 w-full">
@@ -57,68 +81,7 @@ export default function LessonLocation({ form }: { form: any }) {
         </div>
       </div>
 
-      <div className="flex flex-col p-12 w-full rounded-xl shadow-md md:order-2 order-1 ">
-        <Heading
-          title="Ders yerleri"
-          subtitle="Lutfen ders verebileceginiz yerleri secin"
-        />
-        {lessonDistricts && lessonDistricts.length > 0 && (
-          <div className="flex flex-row items-center space-x-3">
-            <Heading
-              title="Secilen ilceler"
-              subtitle={lessonDistricts.join(", ")}
-            />
-          </div>
-        )}
-        <div className="">
-          <FormField
-            control={form.control}
-            name="lessonDistricts"
-            render={() => (
-              <FormItem className="grid grid-cols-2 lg:grid-cols-3 gap-x-4">
-                {districts.map((item: any) => (
-                  <div key={item} className="p-1 mt-2">
-                    <FormField
-                      key={item}
-                      control={form.control}
-                      name="lessonDistricts"
-                      render={({ field }) => {
-                        return (
-                          <FormItem
-                            key={item}
-                            className="flex flex-row items-start space-x-3 space-y-0"
-                          >
-                            <FormControl>
-                              <Checkbox
-                                className="w-5 h-5"
-                                checked={field.value?.includes(item)}
-                                onCheckedChange={(checked) => {
-                                  return checked
-                                    ? field.onChange([...field.value, item])
-                                    : field.onChange(
-                                        field.value?.filter(
-                                          (value: any) => value !== item
-                                        )
-                                      );
-                                }}
-                              />
-                            </FormControl>
-                            <FormLabel className="font-normal">
-                              {item}
-                            </FormLabel>
-                          </FormItem>
-                        );
-                      }}
-                    />
-                  </div>
-                ))}
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-      </div>
+      <LeftSide children={childNode}/>
     </div>
   );
 }
--- src/pages/teacher-register/place.tsx
+++ src/pages/teacher-register/place.tsx
@@ -2,6 +2,7 @@ import { FormCheckbox } from "@/components/form-elements";
 import { useState, useEffect } from "react";
 import { TLessonPlace } from "@/lib/types";
 import Heading from "@/components/heading";
+import LeftSide from "./left-side";
 
 export default function Place({ form }: { form: any }) {
   const [lessonPlaces, setLessonPlaces] = useState<TLessonPlace[]>([]);
@@ -18,14 +19,39 @@ export default function Place({ form }: { form: any }) {
 
   const selectedLessonPlaces = form.watch("lessonPlaces");
 
+  const childNode =      <div className=" md:mr-16 md:w-full bg-rose-100 rounded-3xl flex flex-col md:order-1 order-2 p-6  ">
+  <div>
+    <br />
+    <p>
+      Lütfen ders verebileceginiz ilçeleri seçin. İstediğiniz kadar seçim
+      yapabilirsiniz.
+    </p>
+    <br />
+    <p>
+      Bu bilgiyle sizin ders verebileceğiniz ilçelerdeki öğrencilerle
+      iletişim kurabileceksiniz.
+    </p>
+    <br />
+    <p>
+      Hem siz seçtiğiniz ilçelerdeki öğrencilere gösterileceksiniz, hem de
+      ders taleplerini görürken bu ilçelerdeki kişilere ulaşmış
+      olacaksınız.
+    </p>
+  </div>
+</div>
+
   return (
-    <>
+    <div className="flex flex-col md:flex-row gap-4 md:m-6 w-full">
+      <LeftSide children={childNode}/>
+      <div className="flex md:w-full rounded-xl shadow-md md:order-2 order-1 md:p-16 p-3 flex-col gap-4 pt-4 lg:pt-8">
       <FormCheckbox
         control={form.control}
         placeholder="Vermek istediginiz dersleri secin"
         name="lessonPlaces"
         items={lessonPlaces}
       />
-    </>
+      </div>
+      
+    </div>
   );
 }
